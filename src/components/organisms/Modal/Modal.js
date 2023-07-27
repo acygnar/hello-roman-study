86ba5d94a56b24ea3f731250f0b06f6ff5a5ce73
Adrian Cygnar <developer@klaarity.com>
2023-07-27 21:25:45 +0200

    create basic modal

--- src/components/organisms/Modal/Modal.js
+++ src/components/organisms/Modal/Modal.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import { ModalWrapper } from './Modal.styles.js';
+import { Button } from 'components/atoms/Button/Button';
 
 const modalContainer = document.getElementById('modal-container');
 
-const Modal = () => {
+const Modal = ({ handleClose }) => {
   const modalNode = document.createElement('div');
 
   useEffect(() => {
@@ -14,6 +16,12 @@ const Modal = () => {
     };
   }, []);
 
-  return createPortal(<div>Modal</div>, modalNode);
+  return createPortal(
+    <ModalWrapper>
+      Modal
+      <Button onClick={handleClose}>Close</Button>
+    </ModalWrapper>,
+    modalNode
+  );
 };
 export default Modal;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
@@ -8,14 +8,24 @@ import { Navigate } from 'react-router-dom';
 import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
 import Dashboard from 'views/Dashboard';
 import Modal from 'components/organisms/Modal/Modal';
+import { Button } from 'components/atoms/Button/Button';
 const Root = () => {
+  const [isModalOpen, setModalState] = useState(false);
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTemplate>
-          <Modal />
+          {isModalOpen ? <Modal handleClose={() => setModalState(false)} /> : null}
           <Wrapper>
+            <Button
+              onClick={() => {
+                setModalState(true);
+              }}
+            >
+              Open Modal
+            </Button>
             <Routes>
               <Route exact path="/" element={<Navigate to="/group" />} />
               <Route path="/group/:id?" element={<Dashboard />} />
